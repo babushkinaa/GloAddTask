d762fc6279d735b6b6be95a74ce5a6570a0f6dad
Alexey Babushkin <007-99@mail.ru>
2020-01-28 15:49:16 +0300

    Дополнительное домашнее задание урок 4

--- js/app.js
+++ js/app.js
@@ -1,64 +1,17 @@
-// Необходимо выполнить в отдельном JS файле, подключенному к отдельной HTML странице
-// Переменная lang может принимать 2 значения: 'ru' 'en'.
-// Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
-//     через if, 
-//     через switch-case 
-//     через многомерный массив без ифов и switch.
-// У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-// 	Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-// Запушить проект в репозиторий для усложненных заданий на GitHub
+// let paramses = +prompt('Введите значение: ');
+let paramses = ' Введите значение ghjgjgjgjgjhgasdjgsjgjahdgsjhgjasgjasgdjsag:лодлрыфавлрвыалоравлорлфыоаврлфыварлфоывралоыфвраловыфралофывралвыфрлфыавр ';
 
-
-let lang = 'en';
-// let lang = 'ru';
-
-// namePerson = 'Артем'; //директор
-// namePerson = 'Максим'; // пеподаватель
-namePerson = 'Алексей'; // студент
-
-consoleNamePerson = (namePerson === 'Артем') ? console.log('директор') : 
-(namePerson === 'Максим') ? console.log('преподаватель'):
-(namePerson !== 'Артем' && namePerson !== 'Максим') ? console.log('студент'):null;
-
-
-let weekday = [
-    ['понедельник','вторник','среда','четверг','пятница','суббота','воскресенье'],
-    ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
-];
-
-{
-    console.log( '\nВыводим при помощи  тернарного оператора\n\n');
-    let consoleWekday = (lang === 'ru') ?  weekday[0] : (lang ==='en') ? weekday[1]: '';
-    for (let index = 0; index < consoleWekday.length; index++) {
-        console.log(consoleWekday[index]);
-        
-    }
-}
-{
-    console.log( '\nВыводим при помощи конструкции switch\n\n');
-switch(lang){
-    case ('ru'):
-        for (let index = 0; index < weekday[0].length; index++) {
-            console.log(weekday[0][index]);
+ function sheckParamses(params) {
+     let tmpParamses = params;
+     if (typeof tmpParamses != 'string'){
+         alert(' Вы ввели не строку !!! ')
+     } else {
+        tmpParamses = tmpParamses.trim(tmpParamses);
+        if (tmpParamses.length > 30 ) {
+            tmpParamses = tmpParamses.substring(0,29)+'...';
         }
-        break;
-    case ('en'):
-        for (let index = 0; index < weekday[0].length; index++) {
-            console.log(weekday[1][index]);
-        };
-        break;
-}
-}
-{
-console.log( '\nВыводим при помощи конструкции if\n\n');
+        return tmpParamses;
+     }         
+ }
 
-if (lang === 'ru') {
-    for (let index = 0; index < weekday[0].length; index++) {
-        console.log(weekday[0][index]);
-    }
-} else if (lang === 'en') {
-    for (let index = 0; index < weekday[1].length; index++) {
-        console.log(weekday[1][index]);
-    }
-}
-}
+ console.log( sheckParamses(paramses) );
