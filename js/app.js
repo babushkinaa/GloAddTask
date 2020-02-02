736e30622197896b08984bd47f1b4ebe6b3cc630
Alexey Babushkin <007-99@mail.ru>
2020-02-02 16:35:59 +0300

    Домашнее задание 7 урок дополнительное

--- js/app.js
+++ js/app.js
@@ -1,40 +1,24 @@
-let massiv = [234,543,467,876,999,321,404];
-
-let toStr = function (params) {
-    return params +'';
-}
-let toNum = function (params) {
-    return +params;
-}
-
- for (let index = 0; index < massiv.length; index++) {
-        let number = massiv[index];
-        number = toStr(number);
-        if (toNum(number[0]) === 2 || toNum(number[0]) ===4) {
-            if (toNum(number[0]) === 2 ) {
-                console.log('Начинается на 2 выводим: ',massiv[index]);
-            } else {
-                console.log('Начинается на 4 выводим: ',massiv[index]);
-            }
-            
-        }
-}
+let week = ['вс','пн','вт','ср','чт','пт','сб'];
+let date = new Date();
 
 
+for (let i = 0; i < week.length; i++) {
+    const element = week[i];
 
-
-//https://www.youtube.com/watch?v=MOfrwf0qwJc
-
-let list = [];
-
-newiteration: // ставим метку для выхода из цикла если нашли число дающее остаток 0
-for (var i = 2; i <= 100; i++) {
-  for (var j = 2; j < i; j++) { //прогоняем от 2 до текущего значения i
-    if (i % j == 0) continue newiteration; // нашли число выходим из вложеного цикла и переходим на метку
-  }
-  list.push(i);
-}
-for (let i = 0; i < list.length; i++) {
-    console.log(list[i]);
+    if (i === date.getDay()) {
+        document.write(element.bold()+'</br>')
+    } else if(i === 0 || i === 6 ){
+        document.write(element.italics() +'</br>');
+    }else {
+        document.write(element+'</br>');
+    }
+   
+    
     
 }
+
+
+
+
+
+
