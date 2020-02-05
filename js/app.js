9b1c87fbffeba23d81d4f4ef474f3750a03dae7a
Alexey Babushkin <007-99@mail.ru>
2020-02-05 03:33:22 +0300

    домашнее задание 9 дополнительное

--- js/app.js
+++ js/app.js
@@ -1,24 +1,90 @@
-let week = ['вс','пн','вт','ср','чт','пт','сб'];
-let date = new Date();
 
-
-for (let i = 0; i < week.length; i++) {
-    const element = week[i];
-
-    if (i === date.getDay()) {
-        document.write(element.bold()+'</br>')
-    } else if(i === 0 || i === 6 ){
-        document.write(element.italics() +'</br>');
-    }else {
-        document.write(element+'</br>');
-    }
-   
-    
-    
+function num2str(n, text_forms) {  
+    n = Math.abs(n) % 100; var n1 = n % 10;
+    if (n > 10 && n < 20) { return text_forms[2]; }
+    if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+    if (n1 == 1) { return text_forms[0]; }
+    return text_forms[2];
 }
 
 
 
+// function toAddNull(num){
+//     let sep;
+//     let tmpnum = num;
+
+//     for (let index = 0; index < num.length; index++) {
+//         const element = num[index];
+//         (element === ':')? sep = ':' : (element === '.')? sep ='.':null;
+        
+//     }
+   
+//     res = num.split(/\:|\.|\-/);
+//     let numres ='';
+//     for (let index = 0; index < res.length; index++) {
+//         const element = 1*res[index];
+
+//         if(index !== res.length || res[index] < 10){
+            
+//                 numres += '0'+ res[index]+sep;
+            
+//         }else {
+//             numres += res[index];
+//         }
+
+//     }
+    
+//     return numres.slice(0,-1);
+// }
+// toAddNull('9:5:3');
+// console.log('toAddNull: ', toAddNull('9:5:3'));
+// toAddNull('1.6.2019');
+// console.log('toAddNull: ', toAddNull('1.6.2019'));
+
+// простая функция
+function addNull(nummer){
+    (nummer<10) ? nummer = '0'+nummer:'';
+    return nummer;
+}
+
+let datevar = new Date(),
+    hour = datevar.getHours() + ' '+num2str(datevar.getHours,['час', 'часов', 'часа']),
+    minute = datevar.getMinutes() + ' '+num2str(datevar.getMinutes,['минут', 'минуты', 'минут']),
+    second = datevar.getSeconds()+ ' '+num2str(datevar.getSeconds,['секунд', 'секунды', 'секунд']);
+
+let options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+  timezone: 'UTC',
+
+};
+let time = function(){
+    return addNull(datevar.getHours())+':'+addNull(datevar.getMinutes())+':'+addNull(datevar.getSeconds());
+}
+
+let datez = function(){
+    return addNull(datevar.getDay())+'.'+addNull(datevar.getMonth())+'.'+addNull(datevar.getFullYear());
+}
 
 
 
+document.write( datevar.toLocaleString("ru", options) + ' '+ hour +' '+minute+' '+second+'<br>'); 
+document.write( datez()+ '-' + time()); 
+
+let clock = document.querySelector('.clock');
+console.dir(clock);
+let i =1;
+clock.setAttribute('value',datez()+ '-' + time());
+function showDateTime(){
+        
+        return clock.value = datez()+ '-' + time();
+        // clock.getAttribute('value');
+        console.log('clock: ', clock.getAttribute('value'));
+    
+}
+
+window.setInterval(function(){
+    showDateTime();
+},1000);
