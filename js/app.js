48d09f11fb42e25eb55efc4da98468289c24a97d
Alexey Babushkin <007-99@mail.ru>
2020-03-03 01:55:09 +0300

    26 домашнее задание add

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
 
-    const select = document.getElementById('cars'),
-        output = document.getElementById('output');
+const input = document.querySelector('.input'),
+        output = document.querySelector('.output');
+        
 
-    select.addEventListener('change', () => {
-      fetch('./cars.json')
-        .then((response) => {
-            if (response.status !== 200 ){
-                throw error;
-            }      
-            return (response.json());      
+let url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+
+
+
+input.addEventListener('change', () => {
+    const value = input.value,
+    key = 'trnsl.1.1.20200302T195926Z.c0ebe76fb9a3acfa.e672ceee7f14bca10846e01acd803d036bf65c4b',
+    translate = 'ru-en';
+      fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body:'key='+key+'&lang='+translate+'&text='+value
+        // body:`key=${key}&lang=${translate}&text=${txt}`
+      })
+      .then((response) => {
+                    if (response.status !== 200 ){
+                        throw new Error(response);
+                    }  
+                    return (response.json());      
         })
-        .then(response => {
-            response.cars.forEach(item => {
-                if (item.brand === select.value) {
-                    const {brand, model, price} = item;
-                    output.innerHTML = `Тачка ${brand} ${model} <br>
-                    Цена: ${price}$`;
-                }
-            });
+        .then((response) =>{
+            output.innerHTML = response.text[0];
         })
-        .catch(error => console.error(error));
+        .catch(error => output.innerHTML = error);
     });
 
-});
 
