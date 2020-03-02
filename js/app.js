f3ffaf978907f06402694a81592bac5719e741a0
Alexey Babushkin <007-99@mail.ru>
2020-03-02 16:02:31 +0300

    25 домашнее задание add

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
-button = document.querySelector('.button');
-button1 = document.querySelector('body');
-button.addEventListener('click',start);
-console.log('button: ', button);
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-function start() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-     
-    button1.style.background = color;
-}
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    select.addEventListener('change', () => {
+      fetch('./cars.json')
+        .then((response) => {
+            if (response.status !== 200 ){
+                throw error;
+            }      
+            return (response.json());      
+        })
+        .then(response => {
+            response.cars.forEach(item => {
+                if (item.brand === select.value) {
+                    const {brand, model, price} = item;
+                    output.innerHTML = `Тачка ${brand} ${model} <br>
+                    Цена: ${price}$`;
+                }
+            });
+        })
+        .catch(error => console.error(error));
+    });
+
+});
+
