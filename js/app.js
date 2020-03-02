ad8b01aa94e1e6130ae2c6450628a4c73ec9e97c
Alexey Babushkin <007-99@mail.ru>
2020-03-03 02:09:06 +0300

    26 домашнее задание add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+'use strict';
 
 const input = document.querySelector('.input'),
-        output = document.querySelector('.output');
+        output = document.querySelector('.output'),
+        inputeng = document.querySelector('.input-eng'),
+        outputeng = document.querySelector('.output-eng')
         
 
 let url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
@@ -29,6 +32,28 @@ input.addEventListener('change', () => {
             output.innerHTML = response.text[0];
         })
         .catch(error => output.innerHTML = error);
-    });
+});
 
-
+inputeng.addEventListener('change', () => {
+    const value = inputeng.value,
+    key = 'trnsl.1.1.20200302T195926Z.c0ebe76fb9a3acfa.e672ceee7f14bca10846e01acd803d036bf65c4b',
+    translate = 'en-ru';
+      fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body:'key='+key+'&lang='+translate+'&text='+value
+        // body:`key=${key}&lang=${translate}&text=${txt}`
+      })
+      .then((response) => {
+                    if (response.status !== 200 ){
+                        throw new Error(response);
+                    }  
+                    return (response.json());      
+        })
+        .then((response) =>{
+            outputeng.innerHTML = response.text[0];
+        })
+        .catch(error => outputeng.innerHTML = error);
+});
